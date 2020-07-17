c661be948081051fb9d7a2809b781aa4fa19d2f2
Raphael lins <raphalinns@gmail.com>
2020-07-16 21:44:47 -0300

    Addin another endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,11 @@
 import * as functions from 'firebase-functions';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
+export const helloWorld = functions.https.onRequest((request : any, response: any) => {
   functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+export const realRequest = functions.https.onRequest((request : any, response: any) => {
+  functions.logger.info("Real request!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
